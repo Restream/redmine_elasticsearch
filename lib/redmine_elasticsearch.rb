86b16cc793f145e7092f2e8024220f288b84614b
Danil Tashkinov <danil.tashkinov@gmail.com>
2014-01-28 22:08:52 +0400

    added elastic and serializer folders to autoload path instead explicit require files

--- lib/redmine_elasticsearch.rb
+++ lib/redmine_elasticsearch.rb
@@ -2,7 +2,8 @@ module RedmineElasticsearch
 end
 
 %w{elastic serializers}.each do |fold|
-  Dir[File.dirname(__FILE__) + "/../app/#{fold}/*.rb"].each { |file| require_dependency file }
+  fold_path = File.dirname(__FILE__) + "/../app/#{fold}"
+  ActiveSupport::Dependencies.autoload_paths += [fold_path]
 end
 
 require 'redmine_elasticsearch/patches/redmine_search'
