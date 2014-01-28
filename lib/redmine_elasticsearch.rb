db242f5d61363f69bb0ce09b759bd3906f03d08f
Danil Tashkinov <danil.tashkinov@gmail.com>
2014-01-28 21:58:23 +0400

    moving elastic and serializer folder from lib to app

--- lib/redmine_elasticsearch.rb
+++ lib/redmine_elasticsearch.rb
@@ -2,7 +2,7 @@ module RedmineElasticsearch
 end
 
 %w{elastic serializers}.each do |fold|
-  Dir[File.dirname(__FILE__) + "/#{fold}/*.rb"].each { |file| require file }
+  Dir[File.dirname(__FILE__) + "/../app/#{fold}/*.rb"].each { |file| require_dependency file }
 end
 
 require 'redmine_elasticsearch/patches/redmine_search'
