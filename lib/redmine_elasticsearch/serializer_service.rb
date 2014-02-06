7e964e83e122b7bf43ae9cc81f815c4f307c0148
Danil Tashkinov <danil.tashkinov@gmail.com>
2014-02-06 17:38:21 +0400

    added serializer service which should serialize objects for indexing

---
+++ lib/redmine_elasticsearch/serializer_service.rb
@@ -0,0 +1,24 @@
+class RedmineElasticsearch::SerializerService
+  class << self
+
+    def serialize_to_json(object)
+      object_type = object.class.name.to_sym
+      serializer_klass(object_type).new(object).to_json
+    end
+
+    private
+
+    def serializer_klass(object_type)
+      @serializers ||= {}
+      @serializers[object_type] ||= build_serializer_klass(object_type)
+    end
+
+    def build_serializer_klass(object_type)
+      parent = "#{object_type.to_s.classify}Serializer".safe_constantize || BaseSerializer
+      serializer_klass = Class.new(parent)
+      object_type_schema = RedmineElasticsearch::SchemaRepository.find(object_type)
+      serializer_klass.apply_schema(object_type_schema)
+    end
+
+  end
+end
