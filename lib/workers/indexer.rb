69559c25cc9b0408a97783204ebc0ae757db5306
Danil Tashkinov <danil.tashkinov@gmail.com>
2014-02-08 18:48:17 +0400

    allow to recreate index asynchronously

--- lib/workers/indexer.rb
+++ lib/workers/indexer.rb
@@ -7,25 +7,36 @@ module Workers
     queue :index_queue
     retry_count 3
 
-    def self.defer(object)
-      if object.id?
-        params = { id: object.id, type: object.class.name }
+    def self.defer(object_or_class)
+      if object_or_class.is_a? Class
+        params = { type: object_or_class.name }
+        perform_async(params)
+      elsif object.id?
+        params = { id: object_or_class.id, type: object_or_class.class.name }
         perform_async(params)
       end
     end
 
     def perform(options)
       id, type = options['id'], options['type']
-      klass = type.classify.constantize
-      begin
-        document = klass.find id
-        document.update_index
-      rescue ActiveRecord::RecordNotFound
-        klass.index.remove type, id
-      rescue ::RestClient::Exception, Errno::ECONNREFUSED => e
-        raise IndexError, e, e.backtrace
-      end
+      id.nil? ? update_class_index(type) : update_instance_index(type, id)
     end
 
+    def update_class_index(type)
+      klass = type.classify.constantize
+      klass.update_index
+    rescue ::RestClient::Exception, Errno::ECONNREFUSED => e
+      raise IndexError, e, e.backtrace
+    end
+
+    def update_instance_index(type, id)
+      klass = type.classify.constantize
+      document = klass.find id
+      document.update_index
+    rescue ActiveRecord::RecordNotFound
+      klass.index.remove type, id
+    rescue ::RestClient::Exception, Errno::ECONNREFUSED => e
+      raise IndexError, e, e.backtrace
+    end
   end
 end
