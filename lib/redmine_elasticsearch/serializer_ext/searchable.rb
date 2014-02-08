59ee15ef07196c87649c7441aeba0f4b22f47b16
Danil Tashkinov <danil.tashkinov@gmail.com>
2014-02-08 18:44:06 +0400

    removed unused files

--- lib/redmine_elasticsearch/serializer_ext/searchable.rb
+++
@@ -1,9 +0,0 @@
-module RedmineElasticsearch::SerializerExt::Searchable
-  extend ActiveSupport::Concern
-
-  # search columns added by other plugins
-  def additional_search_columns
-    object.class.searchable_options[:columns]
-  end
-
-end
