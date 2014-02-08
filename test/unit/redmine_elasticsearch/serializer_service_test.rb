310aa0220c96b7aa1d6f482daa6a9d46daacfe10
Danil Tashkinov <danil.tashkinov@gmail.com>
2014-02-08 17:55:38 +0400

    added tests for serializer service

---
+++ test/unit/redmine_elasticsearch/serializer_service_test.rb
@@ -0,0 +1,49 @@
+require File.expand_path('../../../test_helper', __FILE__)
+
+class RedmineElasticsearch::SerializerServiceTest < ActiveSupport::TestCase
+
+  def setup
+    # clear cache
+    RedmineElasticsearch::SerializerService.class_eval do
+      @serializers = nil
+    end
+  end
+
+  def test_get_explicit_serializer
+    issue = Issue.new
+    serializer = RedmineElasticsearch::SerializerService.serializer(issue)
+    assert_kind_of IssueSerializer, serializer
+  end
+
+  def test_serializer_has_additional_attributes
+    Rails.configuration.stubs(:additional_index_properties).returns(
+        :issues => {
+            :foo => { :type => 'string' },
+            :bar => {
+                :properties => {
+                    :name => { :type => 'string' }
+                }
+            }
+        }
+    )
+    issue = Issue.new
+    serializer = RedmineElasticsearch::SerializerService.serializer(issue)
+    assert serializer.respond_to?(:foo)
+    assert serializer.respond_to?(:bar)
+  end
+
+  def test_get_implicit_serializer
+    object = IssueStatus.new
+    serializer = RedmineElasticsearch::SerializerService.serializer(object)
+    assert_kind_of BaseSerializer, serializer
+  end
+
+  def test_serializer_classes_are_cached
+    object = Issue.new
+    serializer1 = RedmineElasticsearch::SerializerService.serializer(object)
+    serializer2 = RedmineElasticsearch::SerializerService.serializer(object)
+    assert_not_equal serializer1, serializer2
+    assert_equal serializer1.class, serializer2.class
+  end
+
+end
