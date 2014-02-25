ca699d446b6972233db751e59ae954c0df2c6e0e
Danil Tashkinov <danil.tashkinov@gmail.com>
2014-02-25 16:24:34 +0400

    added route key for all documents in index

--- app/serializers/base_serializer.rb
+++ app/serializers/base_serializer.rb
@@ -1,5 +1,10 @@
 class BaseSerializer < ActiveModel::Serializer
+  ROUTE_KEY = 'abc'
   self.root = false
 
-  attributes :id
+  attributes :id, :route_key
+
+  def route_key
+    ROUTE_KEY
+  end
 end
