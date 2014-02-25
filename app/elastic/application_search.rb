fe8d325e6e8f83d4c2a57996dd15c326fc1effdb
Danil Tashkinov <danil.tashkinov@gmail.com>
2014-02-25 16:22:17 +0400

    move all types into one index for using parent/child

--- app/elastic/application_search.rb
+++ app/elastic/application_search.rb
@@ -4,8 +4,7 @@ module ApplicationSearch
   included do
     include Tire::Model::Search
 
-    prefix = "#{Rails.application.class.parent_name.downcase}_#{Rails.env}"
-    Tire::Model::Search.index_prefix(prefix)
+    index_name RedmineElasticsearch::INDEX_NAME
 
     after_commit :async_update_index
   end
@@ -20,31 +19,6 @@ module ApplicationSearch
 
   module ClassMethods
 
-    def recreate_index(sync = true)
-      tire.index.delete if tire.index.exists?
-      tire.index.create settings: index_settings, mappings: index_mappings
-      sync ? update_index : async_update_index
-    end
-
-    def index_settings
-      {
-          analysis: {
-              :analyzer => {
-                  :index_analyzer => {
-                      type: 'custom',
-                      tokenizer: 'standard',
-                      filter: %w(lowercase asciifolding russian_morphology english_morphology)
-                  },
-                  :search_analyzer => {
-                      type: 'custom',
-                      tokenizer: 'standard',
-                      filter: %w(lowercase asciifolding russian_morphology english_morphology)
-                  }
-              }
-          }
-      }
-    end
-
     def index_mappings
       { }.merge(additional_index_mappings)
     end
@@ -54,19 +28,14 @@ module ApplicationSearch
       Rails.configuration.additional_index_properties[self.name.tableize.to_sym] || {}
     end
 
-    def update_index
-      searching_scope.find_in_batches do |batch|
-        logger.info "Updating index for #{self.name} (#{batch.length} items)"
-        tire.index.import batch
+    def update_mapping
+      index_mappings.each do |k, v|
+        index.mapping! k, v
       end
     end
 
-    def async_update_index
-      Workers::Indexer.defer(self)
-    end
-
-    def searching_scope
-      self.scoped.includes(searchable_options[:include])
+    def searching_scope(project_id)
+      self.where('project_id = ?', project_id).includes(searchable_options[:include])
     end
   end
 
