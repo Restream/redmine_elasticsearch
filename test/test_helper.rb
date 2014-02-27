aba4640664ef690362dfb3cade0b309d0a5d5efb
Danil Tashkinov <danil.tashkinov@gmail.com>
2014-02-27 22:54:31 +0400

    changed index_settings for test envionment: removed plugin dependancy and changed store type to memory

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,13 +1,24 @@
 require File.expand_path(File.dirname(__FILE__) + '../../../../test/test_helper')
 
-Redmine::Search.available_search_types.each do |search_type|
-  klass = search_type.to_s.classify.constantize
-  klass.class_eval do
-    def self.index_settings
-      { index: { store: { type: :memory } } }
-    end
-  end
-end
+ParentProject.stubs(:index_settings).returns(
+    {
+        index: { store: { type: :memory } },
+        analysis: {
+            analyzer: {
+                :index_analyzer => {
+                    type: 'custom',
+                    tokenizer: 'standard',
+                    filter: %w(lowercase)
+                },
+                :search_analyzer => {
+                    type: 'custom',
+                    tokenizer: 'standard',
+                    filter: %w(lowercase)
+                }
+            }
+        }
+    }
+)
 
 class ActiveSupport::TestCase
   def search_all_for_klass(klass, user, options = {})
