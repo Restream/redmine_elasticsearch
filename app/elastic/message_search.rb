e3eb27b34102d81d3737dc79c159f5f623216182
Danil Tashkinov <danil.tashkinov@gmail.com>
2014-02-28 19:42:25 +0400

    mapping acts_as_event fields for messages

--- app/elastic/message_search.rb
+++ app/elastic/message_search.rb
@@ -17,16 +17,18 @@ module MessageSearch
       {
           id: { type: 'integer' },
           project_id: { type: 'integer', index: 'not_analyzed' },
+          route_key: { type: 'string', not_analyzed: true },
 
-          subject: { type: 'string' },
-          content: { type: 'string' },
+          # acts_as_event fields
+          created_on: { type: 'date', index_name: 'datetime' },
+          subject: { type: 'string', index_name: 'title' },
+          content: { type: 'string', index_name: 'description' },
           author: { type: 'string' },
+          url: { type: 'string', index: 'not_analyzed' },
+          type: { type: 'string', index: 'not_analyzed' },
 
-          created_on: { type: 'date' },
           updated_on: { type: 'date' },
-          replies_count: { type: 'date', index: 'not_analyzed' },
-          route_key: { type: 'string', not_analyzed: true }
-
+          replies_count: { type: 'date', index: 'not_analyzed' }
       }.merge(additional_index_mappings)
     end
 
