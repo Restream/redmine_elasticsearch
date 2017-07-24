d0bc13011bad34caaadff76748160d9129cff256
Danil Tashkinov <danil.tashkinov@gmail.com>
2017-07-25 00:27:50 +0300

    Update elasticsearch version for travis-ci

--- init.rb
+++ init.rb
@@ -4,7 +4,7 @@ Redmine::Plugin.register :redmine_elasticsearch do
   name        'Redmine Elasticsearch Plugin'
   description 'This plugin integrates the Elasticsearch full-text search engine into Redmine.'
   author      'Restream'
-  version     '0.2.0'
+  version     '0.2.1'
   url         'https://github.com/Restream/redmine_elasticsearch'
 
   requires_redmine version_or_higher: '2.1'
